ad2a7425fe9eda572882edab0d353f0da5f1bb1b
pranavthakur404 <pranavufc@gmail.com>
2024-02-06 19:40:46 +0530

    update products.js

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import useFetch from "../Hooks/useFetch";
+import useFetch from "../hooks/useFetch";
 import Product from "./Product";
 import styles from "../component_css/Products.module.css";
 
